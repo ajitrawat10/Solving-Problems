17cd90044eb59da35f9e4086f293e03941a3f1d1
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-02-15 10:24:36 +0530

    Update fibonacci.cpp

--- Algorithms/fibonacci.cpp
+++ Algorithms/fibonacci.cpp
@@ -22,3 +22,29 @@ int main(){
    cout<<fib(n);
 
  }
+
+
+
+/*
+ Second method
+ It is an Memoization way to find nth fibonacci number.
+ Its time complexity is O(n).
+ */
+
+ #include<iostream>
+ using namespace std;
+ int fib(int n){
+     int a[n+1];
+   a[1]=1;
+   a[2]=1;
+ for (int i=3;i<=n;i++){
+    a[i]=a[i-1]+a[i-2];
+ }
+ return a[n];
+
+ }
+ int main(){
+   int n=5;
+   cout<<fib(n);
+ }
+
